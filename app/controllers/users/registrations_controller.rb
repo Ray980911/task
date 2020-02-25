52e424588165f8b81e4d7e5084f5d2d8ad75d239
Ray980911 <rayhori0911@gmail.com>
2020-02-25 21:32:09 +0900

    heroku

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -11,27 +11,23 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def show
 
   end
-  # GET /resource/sign_up
+
   # def new
-  #   super
+
   # end
 
-  # POST /resource
   # def create
   #   super
   # end
 
-  # GET /resource/edit
   # def edit
   #   super
   # end
 
-  # PUT /resource
   # def update
   #   super
   # end
 
-  # DELETE /resource
   # def destroy
   #   super
   # end
