e704f8bdd71be960091de5deed76b97739fbd15d
Ray980911 <Rayreds1830@gmail.com>
2020-03-04 19:04:41 +0900

    validation

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,6 +2,11 @@ class Task < ApplicationRecord
   belongs_to :user
   enum status: { 未対応: 0, 対応中: 1, 完了: 2 } 
   paginates_per 10
+  validates :user_id, presence: true
+  validates :title, presence: true, length: { maximum: 30 }
+  validates :content, presence: true, length: { maximum: 140 }
+  validates :status, presence: true
+
   scope :index_all, -> {
     select(:id, :title, :content, :created_at, :deadline, :status, :user_id).order(created_at: :asc).includes(:user)
   }
