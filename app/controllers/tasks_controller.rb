04d428e5da2b5ed0c60434c5b6915083ce9f71f6
Ray980911 <rayhori0911@gmail.com>
2020-03-09 14:21:48 +0900

    authenticate_user

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
 
   def index
     @tasks = Task.index_all.page(params[:page])
@@ -28,35 +28,35 @@ class TasksController < ApplicationController
     @task = Task.new(task_params)
     @task.user = current_user
       if @task.save
-        redirect_to @task, notice: 'Task was successfully created.' 
+        redirect_to @task, notice: 'Task was successfully created.'
       else
-        render :new 
+        render :new
       end
   end
 
   def update
-    if @task.user_id == current_user.id 
+    if @task.user_id == current_user.id
         if @task.update(task_params)
           redirect_to @task, notice: 'Task was successfully updated.'
         else
-          render :edit 
+          render :edit
         end
     else
       redirect_to tasks_path
-    end  
+    end
   end
 
   def destroy
     if @task.user_id == current_user.id
       @task.destroy
-      redirect_to tasks_url, notice: 'Task was successfully destroyed.' 
-    else  
-      redirect_to tasks_path  
+      redirect_to tasks_url, notice: 'Task was successfully destroyed.'
+    else
+      redirect_to tasks_path
     end
   end
 
   private
-  
+
   def set_task
     @task = Task.find(params[:id])
   end
